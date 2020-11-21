abf1281959ba4b5fc5fe3e1204d814334366b8ec
Basem Emara <contact@basememara.com>
2020-11-21 00:26:18 -0500

    use relative path for redirect

--- Website/success.php
+++ Website/success.php
@@ -1,4 +1,4 @@
 <?php
-     header('Location: http://zamzam.io/tests/autofill/redirect.html?token=123&code=abc&callback_url_scheme=' . $_GET['redirect']);
+     header('Location: redirect.html?token=123&code=abc&callback_url_scheme=' . $_GET['redirect']);
      exit();
 ?>
